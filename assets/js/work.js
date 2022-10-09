34891e071ca6091d9db404b06dce668c65b6223c
Jeongwook Oh <hiphophammer@hotmail.com>
2022-10-09 01:11:40 -0700

    jquery for loading work review

---
+++ assets/js/work.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+	$("#target").load("https://www.upwork.com/freelancers/~019afa74078924c379 #main>div>div>div>div>div>div.up-card.py-0.my-0.d-none.d-lg-block>section.row>div.col.min-width-0>section.up-card-section.py-30.work-history-section");
+});
